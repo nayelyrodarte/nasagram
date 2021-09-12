9c252dda068d76ad8942bd0477af6cf9a8ad1a5d
Nayely Rodarte <nayely@coolhumans.team>
2021-09-11 21:16:22 -0400

    Add initial api fetching @ App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      "https://api.nasa.gov/EPIC/api/natural?api_key=XtvWaKCFwjrT48ikMygia8yNi8a3KUm88sD2aMcL"
+    )
+      .then((res) => res.json().then((result) => setData(result)))
+      .catch((e) => console.error(e));
+
+    // return () => {
+    //   cleanup
+    // }
+  }, []);
+
   return <div></div>;
 }
 

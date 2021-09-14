241b3a41eea0edde8cb46fa0e88d2bd7053edbcb
Nayely Rodarte <nayely@coolhumans.team>
2021-09-14 19:57:27 -0400

    Render data in post components

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Post() {
-  return <article className="post"></article>;
+function Post({ data }) {
+  console.log(data);
+
+  const { identifier, date, image, caption } = data;
+
+  let dateQuery = date.replaceAll("-", "/").slice(0, 10);
+
+  let url = `https://epic.gsfc.nasa.gov/archive/natural/${dateQuery}/png/${image}.png`;
+
+  return (
+    <article className="post">
+      <p>{date}</p>
+      <img src={url} alt={caption} style={{ width: "5em" }}></img>
+    </article>
+  );
 }
 
 export default Post;

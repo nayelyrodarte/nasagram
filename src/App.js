98fcc376565f180800c3889385079b96b0c8e913
Nayely Rodarte <nayely@coolhumans.team>
2021-09-20 20:55:51 -0400

    Add loading state in App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Post from "./Post";
 function App() {
   const [data, setData] = useState([]);
   const [likes, setLikes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(
       "https://api.nasa.gov/EPIC/api/natural?api_key=XtvWaKCFwjrT48ikMygia8yNi8a3KUm88sD2aMcL"
     )
-      .then((res) => res.json().then((result) => setData(result)))
+      .then((res) =>
+        res.json().then((result) => {
+          setData(result);
+          setLoading(false);
+        })
+      )
       .catch((e) => console.error(e));
 
     // return () => {
@@ -30,14 +37,28 @@ function App() {
     }
   }, [data]);
 
+  if (loading) {
+    return "Loading";
+  }
+
   return (
     <main>
       <h1>NASAgram</h1>
       <section className="feed">
         {data?.map((item, index) => (
-          <Post data={item} likes={likes} key={`post-${index}`} />
+          <Post
+            data={item}
+            likes={likes}
+            key={`post-${index}`}
+            loading={loading}
+          />
         ))}
       </section>
+      <footer>
+        <p>Made by Nayely Rodarte</p>
+        <p>for Shopify's Winter 2021 Front End Internship Challenge</p>
+        <p>nayelyrodarte.com</p>
+      </footer>
     </main>
   );
 }
